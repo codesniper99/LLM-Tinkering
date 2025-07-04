31aff86efa52b9e254918cf6e7e5cb48394770bb
Akhil Vaid <akhilvaid21@gmail.com>
2025-07-04 13:45:14 -0700

    Moved to uv, WSL

---
+++ LLM with RAG/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from llm-with-rag!")
+
+
+if __name__ == "__main__":
+    main()
--- LLM with RAG/src/lmstudio.py
+++ LLM with RAG/src/lmstudio.py
@@ -1,12 +1,17 @@
+import queue
 from typing import List
 from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
+import nemo.collections.asr as nemo_asr
+from nemo.collections.asr.models import ASRModel, EncDecCTCModelBPE 
+# from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
 import chromadb
 from huggingface_hub import login
 from dotenv import load_dotenv
 import os
+import numpy as np
+import sounddevice as sd
 import argparse
 import torch
 
@@ -118,82 +123,145 @@ def persist_in_collection(docs: List[Document], embeddings):
 
 # def retrieve_context():
 #     pass
+def get_question_by_text():
+    query = input("\nAsk a question (or type 'exit'): ")
+    if query.lower() in ['exit', 'quit']:
+        return "exit"
+    return query
 
-if __name__ == "__main__":
 
+
+
+def get_query_by_audio():
+    
+    print("Inside")
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    asr_model = nemo_asr.models.EncDecCTCModelBPE.from_pretrained(model_name="nvidia/parakeet-ctc-0.6b", map_location=device)
+    
+
+    print(f"ASR Model {asr_model}")
+    query = "What is this paper about"
+    SAMPLE_RATE = 16000
+    BLOCK_SIZE = 1024
+    CHANNELS = 1
+    BUFFER_DURATION = 1.0  # seconds of audio per inference
+    OVERLAP = 0.5          # seconds overlap between chunks
+    print(dir(asr_model))
+    audio_q = queue.Queue()
+
+    def audio_callback(indata, frames, time, status):
+        if status:
+            print("Stream status:", status)
+        audio_q.put(indata.copy())
+
+    buffer = np.zeros(int(SAMPLE_RATE * BUFFER_DURATION), dtype=np.float32)
+    overlap_samples = int(OVERLAP * SAMPLE_RATE)
+
+    with sd.InputStream(samplerate=SAMPLE_RATE, channels=CHANNELS,
+                        dtype='float32', callback=audio_callback):
+        print("🎤 Listening... Ctrl+C to stop")
+        while True:
+            try:
+                chunk = audio_q.get()
+                chunk = chunk.flatten()
+
+                # Roll buffer and append new data
+                buffer = np.roll(buffer, -len(chunk))
+                buffer[-len(chunk):] = chunk
+
+                # Run transcription every BUFFER_DURATION seconds
+                audio_input = buffer.copy()
+                text = asr_model.transcribe([audio_input])[0] # type:  ignore
+                print("🗣️", text)
+
+            except KeyboardInterrupt:
+                print("🛑 Stopped.")
+                break
+
+    return query
+def argument_parsing():
     parser  = argparse.ArgumentParser(description="Choose LLM Backend")
     parser.add_argument("--llm", type=str, choices=["openai", "mistral"], default="mistral", help="Which LLM to use: 'openai' or 'mistral'")
     args = parser.parse_args()
-    # print(torch.cuda.is_available())
-    # print(torch.__version__)
-    # print(torch.cuda.is_available())
-    # print(torch.cuda.get_device_name(0)) 
-    
+    return args
+
+if __name__ == "__main__":  
+    print(sd.query_devices())
+    args = argument_parsing()
     load_dotenv()
     # openai_api_key = os.getenv("OPENAI_API_KEY")
 
     pages = load_paper_pdf()
     docs, embeddings = chunk_and_embed_document(pages)
     collection = persist_in_collection(docs=docs, embeddings=embeddings)
-    # if args.llm == "openai":
-    #     rag_chain = setup_openai_llm_chain(vector_store=vector_store, openai_api_key=openai_api_key)
-    # else:
-    #     rag_chain = setup_mistral_llm_chain(vector_store=vector_store, openai_api_key=openai_api_key)
 
-
-    
-    
+    query = get_query_by_audio()
     # # --- Step 6: Ask interactively ---
-    while True:
-        query = input("\nAsk a question (or type 'exit'): ")
-        if query.lower() in ['exit', 'quit']:
-            break
-        embedding_response = openai_client.embeddings.create(
-            model="text-embedding-nomic-embed-text-v1.5",
-            input=query,
-            encoding_format="float"
-        )
-        query_embedding = embedding_response.data[0].embedding
+    # while True:
+    #     # query = get_question_by_text()
         
-        rag_result = collection.query(
-            n_results=5, 
-            query_embeddings=query_embedding
-            )
-        print(type(rag_result['documents']))
 
-        # chunks = [doc for doc in rag_result['documents'][0]]
-        chunks = [doc.page_content for doc in docs]
-        context = "\n\n".join(chunks)
-        prompt = f"""You are an expert assistant. Use the following context which is based on a research paper to answer the question which follows it. Context is wrapped in << and >>, question is wrapped by < and >
-        The user's questions will be related to this paper provided in the context.
+        
+    #     if query == "exit":
+    #         break
+        
+    #     embedding_response = openai_client.embeddings.create(
+    #         model="text-embedding-nomic-embed-text-v1.5",
+    #         input=query,
+    #         encoding_format="float"
+    #     )
+    #     query_embedding = embedding_response.data[0].embedding
+        
+    #     rag_result = collection.query(
+    #         n_results=5, 
+    #         query_embeddings=query_embedding
+    #         )
+    #     # print(type(rag_result['documents']))
 
-        Context:
-        <<{context}>>
+    #     # chunks = [doc for doc in rag_result['documents'][0]]
+    #     chunks = [doc.page_content for doc in docs]
+    #     context = "\n\n".join(chunks)
+    #     prompt = f"""You are an expert assistant. Use the following context which is based on a research paper to answer the question which follows it. Context is wrapped in << and >>, question is wrapped by < and >
+    #     The user's questions will be related to this paper provided in the context.
 
-        Question: <{query}>
-        Answer:"""
-        print(f"Prompt is {prompt}")
+    #     Context:
+    #     <<{context}>>
 
-        response = openai_client.chat.completions.create(
-            model="google/gemma-3-1b",
-            messages=[
-                {"role": "system", "content": "You are helpful AI Assistant!"},
-                {"role": "user", "content": prompt},
-            ],
-            temperature=0
-        )
-        print(response)
-        print("\n\n Answer: \n")
-        ai_response = response.choices[0].message.content
-        print(response.choices[0].message.content)
-        print("Response received.")
+    #     Question: <{query}>
+    #     Answer:"""
+    #     print(f"Prompt is {prompt}")
 
-        query_embedding_2 = openai_client.embeddings.create(
-            model="text-embedding-nomic-embed-text-v1.5",
-            input=ai_response,
-            encoding_format="float"
-        )
-        rag_result_2 = collection.query(n_results=1, query_embeddings=query_embedding_2.data[0].embedding)
-        chunks_2 = [doc for doc in rag_result_2['documents'][0]]
-        answer = "\n\n".join(chunks_2)
-        print(f"Grounding Reference: {answer}")
+    #     response = openai_client.chat.completions.create(
+    #         model="google/gemma-3-1b",
+    #         messages=[
+    #             {"role": "system", "content": "You are helpful AI Assistant!"},
+    #             {"role": "user", "content": prompt},
+    #         ],
+    #         temperature=0,
+    #         stream=True
+    #     )
+    #     streamed_response = ""
+    #     print("\n\nAnswer (streaming):\n")
+    #     for chunk in response:
+    #         # print(chunk.choices[0].delta.content)
+    #         if chunk.choices and chunk.choices[0].delta and chunk.choices[0].delta.content:
+    #             token = chunk.choices[0].delta.content
+    #             print(token, end="", flush=True)
+    #             streamed_response += token
+                
+    #     # print("\n\nResponse received.")
+    #     # print(response)
+    #     # print("\n\n Answer: \n")
+    #     # ai_response = response.choices[0].message.content
+    #     # print(response.choices[0].message.content)
+    #     # print("Response received.")
+
+    #     # query_embedding_2 = openai_client.embeddings.create(
+    #     #     model="text-embedding-nomic-embed-text-v1.5",
+    #     #     input=ai_response,
+    #     #     encoding_format="float"
+    #     # )
+    #     # rag_result_2 = collection.query(n_results=1, query_embeddings=query_embedding_2.data[0].embedding)
+    #     # chunks_2 = [doc for doc in rag_result_2['documents'][0]]
+    #     # answer = "\n\n".join(chunks_2)
+    #     # print(f"Grounding Reference: {answer}")
